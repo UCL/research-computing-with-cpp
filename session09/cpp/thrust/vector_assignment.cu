bab0206798730969363ef4cbb6702b900dcaa446
Jens Hedegaard Nielsen <jens.nielsen@ucl.ac.uk>
2015-03-09 18:39:05 +0000

    add missing examples of thrust code

--- session09/cpp/thrust/vector_assignment.cu
+++ session09/cpp/thrust/vector_assignment.cu
@@ -1,1 +1,3 @@
-### Content
+thrust::device_vector<int> D(4,0);
+D[0] = 99; 
+D[1] = 88;
--- session09/cpp/thrust/vector_copy.cu
+++ session09/cpp/thrust/vector_copy.cu
@@ -1,1 +1,19 @@
-### Put content here
+/// "copy"
+
+thrust::host_vector<int> H1;
+thrust::host_vector<int> H2(H1);
+
+/// "template_copy"
+
+thrust::host_vector<float> H(5);
+thrust::host_vector<double> D(H);
+
+/// "transfer_copy"
+
+thrust::host_vector<int> H;
+thrust::device_vector<int> D(H);
+
+/// "stl_copy"
+
+std::vector<int> stl_vector;
+thrust::device_vector<int> D(stl_vector);
